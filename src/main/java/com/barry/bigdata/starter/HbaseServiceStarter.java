df0efab4ae3eb67845ec619402b8ed2ae088bb4c
hants <htbyang2@163.com>
2018-05-11 22:17:40 +0800

    hbases integrate springboot

---
+++ src/main/java/com/barry/bigdata/starter/HbaseServiceStarter.java
@@ -0,0 +1,25 @@
+package com.barry.bigdata.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 描述: 系统启动主方法
+ * @SpringBootApplication等同于 @Configuration @EnableAutoConfiguration @ComponentScanpublic
+ *
+ * @outhor hants
+ * @create 2018-05-01 下午5:29
+ */
+@SpringBootApplication
+@ComponentScan(basePackages={"com.barry.bigdata"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HbaseServiceStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbaseServiceStarter.class, args);
+    }
+
+}
