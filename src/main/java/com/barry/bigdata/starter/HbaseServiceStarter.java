b9ea8f13ae3501a35f5036f4ae53592b784e85b7
hants <htbyang2@163.com>
2018-05-11 23:11:03 +0800

    init commit

--- src/main/java/com/barry/bigdata/starter/HbaseServiceStarter.java
+++ src/main/java/com/barry/bigdata/starter/HbaseServiceStarter.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 描述: 系统启动主方法
